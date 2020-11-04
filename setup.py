55aa75eda09a94ea0deb8878633cf8342ba0c91c
leguilln <nicolas.leguillarme@univ-grenoble-alpes.fr>
2020-11-04 12:23:08 +0100

    commit new project generated using cookiecutter-python-cli

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+A deep neural model for taxonomic entity recognition
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='detaxer',
+    version='0.1.0',
+    url='https://github.com/nleguillarme/python-detaxer',
+    license='BSD',
+    author='Nicolas Le Guillarme',
+    author_email='nicolas.leguillarme@univ-grenoble-alpes.fr',
+    description='A deep neural model for taxonomic entity recognition',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'detaxer = detaxer.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
